eed7a13ba9bc9999c332d1097b7aa57e33237ccb
DAVI-REZENDE <daviresendes12@gmail.com>
2021-01-23 18:19:14 -0400

    add clone tesla, finish scroll

---
+++ clone-tesla/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Page from './components/Page'
+
+import { GlobalStyles } from './styles/GlobalStyles';
+
+function App() {
+  return (
+
+    <>
+      <Page/>
+
+
+      <GlobalStyles/>
+    </>
+
+  );
+}
+
+export default App;
---
+++ clone-tesla/src/components/Page/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { ModelsWrapper, ModelSection } from "../Model";
+import DefaultOverlayContent from "../DefaultOverlayContent";
+
+import { Container } from "./styles";
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      <ModelsWrapper>
+        
+        <div>
+          {[
+            "Model One",
+            "Model Two",
+            "Model Three",
+            "Model Four",
+            "Model Five",
+            "Model Six",
+            "Model Seven",
+          ].map((modelName) => (
+            <ModelSection
+              key={modelName}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            ></ModelSection>
+          ))}
+        </div>
+        
+      </ModelsWrapper>
+    </Container>
+  );
+};
+
+export default Page;
